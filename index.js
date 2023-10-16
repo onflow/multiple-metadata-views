9ebf436f16ce31ed1682a2ecb9c2d69cdba208f5
lealobanov <lea.lobanov@icloud.com>
2023-10-16 23:13:04 +0900

    Add README template and license

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ export const multipleMetadataViews = {
   playgroundLink:
     "https://play.onflow.org/41befd2d-31f3-47f0-ae30-aad776961e31?type=script&id=baf002e4-7124-4ffb-894e-5c04e95629de",
   excerpt:
-    "Have more views you want to create for metadata? This is how you create new views in your smart contract and how you execute a transaction to display them all",
+    "Have more views you want to create for metadata? This is how you create new views in your smart contract and how you execute a transaction to display them all.",
   smartContractCode: contractPath,
   smartContractExplanation: smartContractExplanationPath,
   transactionCode: transactionPath,
